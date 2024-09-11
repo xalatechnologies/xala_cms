5b61ad16aeb7d901985da5b3c8c4713341e05f6d
Ibrahim Rahmani <ibrahim@xala.no>
2024-09-11 13:44:25 +0200

    Separate root

--- public/assets/dashboard/js/ckeditor/plugins/toc/lang/ar.js
+++
@@ -1,5 +0,0 @@
-﻿CKEDITOR.plugins.setLang( 'toc', 'ar', {
-	tooltip : 'إنشاء فهرس الموضوع',
-	notitles: 'لايوجد عناوين رئيسية! \nPlease format some text as "Heading", "Heading 2" etc. within the editor',
-	ToC: 'فهرس الموضوع'
-});
--- public/assets/frontend/vendor/intl-tel-input/js/i18n/hi/index.js
+++
@@ -1,6 +0,0 @@
-//* THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
-import countryTranslations from "./countries.js";
-import interfaceTranslations from "./interface.js";
-
-export { countryTranslations, interfaceTranslations };
-export default { ...countryTranslations, ...interfaceTranslations };
